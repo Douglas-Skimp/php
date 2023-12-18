a4397ec8f5f0fe3add009d7fbf954f65a95fdfb0
Douglas-Skimp <douglas.rodrigues.a@gmail.com>
2023-12-17 21:27:21 -0300

    First commit

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import http from 'node:http'
 import { json } from './middleware/json.js'
 import { routes } from './routes.js'
 
+// Query Parameters
+// Route Parameters
+// Request Body
+
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
