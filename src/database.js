85e78c363c3e792936f856abda25d819a1b277b3
Douglas-Skimp <douglas.rodrigues.a@gmail.com>
2023-12-26 18:45:53 -0300

    First commit

--- src/database.js
+++ src/database.js
@@ -38,4 +38,16 @@ export class Database {
 
     return data;
   }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    // Percorre cada um dos registros(cada item que tem dentro do array), 
+    // procurando se existe um usuario que tenha o id igual ao id que eu estou
+    // busacando, se existir ele irá me informar qual o indice dessa informação
+    
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
 }
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,13 @@
 // /users/:id
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1[a-z0-9\-_]+)')
 
-  console.log(path.machAll(routeParametersRegex))
+  // console.log(pathWithParams)
+
+  // const test = /\/users\/([a-z0-9-_]+)/
+
+  const pathRegex = new RegeExp(`^${pathWithParams}`)
+
+  return pathRegex
 }
